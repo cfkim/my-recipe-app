b76f231a9c9e027b9a55f035ef3b822b3734d184
cfkim <christine.fth.kim@gmail.com>
2024-07-18 15:41:51 -0400

    Initial Commit

---
+++ RecipeBrowser/RecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserApp.swift
+//  RecipeBrowser
+//
+//  Created by Christine Kim on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
